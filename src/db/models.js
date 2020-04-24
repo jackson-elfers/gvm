f2e14a9cc8ef6fbee904b26746e88d834fb097e7
jackson-elfers <jacksonelfersdev@outlook.com>
2020-04-23 20:56:45 -0700

    add: modify user model deduct unique

--- src/db/models.js
+++ src/db/models.js
@@ -4,7 +4,7 @@ const db = require("./index");
 const user = async function() {
   const query = `
 create table if not exists user(
-_id binary(16) not null unique,
+_id binary(16) not null,
 created_at datetime not null,
 updated_at datetime not null,
 username varchar(255) not null unique,
